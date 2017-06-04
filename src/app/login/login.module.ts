548691379727bde3e67f804033be327475d58455
thieu_nx <xuanhoa97@gmail.com>
2017-06-04 15:45:36 +0700

    D6 : Login and logout

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { AuthenService } from '../core/services/authen.service';
+import { NotificationService } from '../core/services/notification.service';
 export const routes: Routes = [
    //localhost:4200/login
   { path: '', component: LoginComponent }
@@ -9,8 +12,10 @@ export const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
+  providers:[AuthenService,NotificationService],
   declarations: [LoginComponent]
 })
 export class LoginModule { }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { SystemConstants } from '../core/common/system.constants';
+import { UrlConstants } from '../core/common/url.constants';
+import { UtilityService } from '../core/services/utility.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private utilityService: UtilityService) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    localStorage.removeItem(SystemConstants.CURRENT_USER);
+    this.utilityService.navigate(UrlConstants.LOGIN);
+  }
+
+  gotoHome() {
+    this.utilityService.navigate(UrlConstants.HOME);
   }
 
 }
